926815f666ca99f69a7581861ff3c4f18568329c
Noah Canter <noahpinion98@proton.me>
2026-02-05 10:41:17 -0600

    send_from_directory

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+#!/usr/bin/env python3
+
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home_page():
-    return "Hello world!"
+    return send_from_directory("static", "index.html")
 
 
 if __name__ == "__main__":
